8f66f45172fea97f12aee682f461dfbfbf356b44
Priyan66 <priyanshuvs66@gmail.com>
2026-02-16 09:58:29 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String gitUsername = "YourGitHubUsername";
+        String gitEmail = "your-email@example.com";
+
+        System.out.println("Git Username: " + gitUsername);
+        System.out.println("Git Email: " + gitEmail);
+    }
+}
